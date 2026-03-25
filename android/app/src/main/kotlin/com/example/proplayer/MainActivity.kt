a4943d1515022d09ecb53580fc8bfaf6d1c1c277
Wada Negassa <promisehispeace@gmail.com>
2026-03-25 10:08:56 +0300

    feat(android): update AndroidManifest and MainActivity configuration

--- android/app/src/main/kotlin/com/example/proplayer/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/proplayer/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.proplayer
 
-import io.flutter.embedding.android.FlutterActivity
+import com.ryanheise.audioservice.AudioServiceFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : AudioServiceFragmentActivity()
